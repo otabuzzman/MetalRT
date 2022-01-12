d6fb47e7462c1dfbdc454f8269197bc0557e3b3e
otabuzzman <iuergen.schuck@gmail.com>
2022-01-12 14:56:00 +0100

    Remove custom modifiers (for the time being)

--- Sources/MetalRT/MRTView.swift
+++ Sources/MetalRT/MRTView.swift
@@ -21,20 +21,3 @@ struct MRTView: UIViewRepresentable {
     func updateUIView(_ uiView: Content, context: Context) {
     }
 }
-
-// https://www.hackingwithswift.com/books/ios-swiftui/custom-modifiers
-
-struct MRTLoadScene: ViewModifier {
-    var resourceArray: [String]?
-    
-    func body(content: Content) -> some View {
-        content
-            .loadScene(fromObjResourceArray resourceArray: [String]?)
-    }
-}
-
-extension View {
-    func loadScene(fromObjResourceArray resourceArray: [String]?) -> some View {
-        modifier(MRTLoadScene(resourceArray: resource))
-    }
-}
