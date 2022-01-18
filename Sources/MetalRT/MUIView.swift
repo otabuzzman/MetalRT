ef55720399bba5ac800376719828f8774620734d
Jürgen Schuck <otabuzzman@users.noreply.github.com>
2022-01-18 09:59:23 +0100

    Fix access levels

--- Sources/MetalRT/MUIView.swift
+++ Sources/MetalRT/MUIView.swift
@@ -1,17 +1,17 @@
 import MetalKit
 import SwiftUI
 
-public ￼struct MUIView<Content>: UIViewRepresentable where Content: MRTView {
+public struct MUIView<Content>: UIViewRepresentable where Content: MRTView {
     var mrtView: Content
     
     init(closure: () -> Content) {
         mrtView = closure()
     }
     
-    func makeUIView(context: Context) -> Content {
+    public func makeUIView(context: Context) -> Content {
         return mrtView
     }
     
-    func updateUIView(_ uiView: Content, context: Context) {
+    public func updateUIView(_ uiView: Content, context: Context) {
     }
 }
