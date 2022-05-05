0e95e5d8d193d125c495086852c7b42bb2ef97c1
otabuzzman <iuergen.schuck@gmail.com>
2022-05-05 20:19:49 +0200

    Update file structure

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "MetalRT",
+    name: "MetalRTOWOW",
     platforms: [
         .iOS(.v15),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "MetalRT",
-            targets: ["MetalRT"]),
+            name: "MetalRTOW",
+            targets: ["MetalRTOW"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,10 +22,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "MetalRT",
+            name: "MetalRTOW",
             dependencies: []),
         .testTarget(
-            name: "MetalRTTests",
-            dependencies: ["MetalRT"]),
+            name: "MetalRTOWTests",
+            dependencies: ["MetalRTOW"]),
     ]
 )
